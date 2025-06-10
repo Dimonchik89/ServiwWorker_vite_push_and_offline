0dfbe4cae0971bdbd1d6f44d6ea470c1f8780c5d
Dmitriy Kislitsyn <dima.music1989@gmail.com>
2025-06-10 09:57:58 +0300

    removed worker registration in main.js because it happens in the subscribeUser function in the App.js

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-if ('serviceWorker' in navigator) {
-  // navigator.serviceWorker.register('/sw.js') // для продакшин режима
-  navigator.serviceWorker.register('/sw-dev.js') // для тестов в режиме разработки
-    .then(reg => {
-      console.log('Service worker registered:', reg);
-    });
-}
+// if ('serviceWorker' in navigator) {
+//   // navigator.serviceWorker.register('/sw.js') // для продакшин режима
+//   navigator.serviceWorker.register('/sw-dev.js') // для тестов в режиме разработки
+//     .then(reg => {
+//       console.log('Service worker registered:', reg);
+//     });
+// }
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
